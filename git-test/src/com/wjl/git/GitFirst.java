29ccf15e87ee50d408766ed07d1ca6b4794e779b
wjl <wangjianlong@126.com>
2019-04-23 22:50:17 +0800

    修改1

--- git-test/src/com/wjl/git/GitFirst.java
+++ git-test/src/com/wjl/git/GitFirst.java
@@ -4,4 +4,10 @@ public class GitFirst {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test()
+    {
+        int i=0;
+        i +=10;
+    }
 }
